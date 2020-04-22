dcd5244adbbe059a0124248d8d8998a44c54e8f5
姜枨文 <63992597+jiangcw08@users.noreply.github.com>
2020-04-22 10:06:50 +0800

    vue语法复习

--- src/components/test.vue
+++ src/components/test.vue
@@ -3,13 +3,54 @@
   <div>
       <BackTop :target="getTarget" :bottom="0" :right="50"></BackTop>
 
-      a
+      <!-- v-html 可以识别htm标签 -->
+      <div v-html="msg">{{ msg }}</div>
+      {{ msg }}
+
+      <h2 v-show="ok">Hellow word</h2>
+
+      <h2 v-if="ok">Hellow word</h2>
+
+      {{ resver_msg }}
+
+      <p>{{ counter }}
+
+        <Button @click="counter++">+</Button>
+      </p>
+
+
+
+
+
   </div>
 </template>
 
 <script>
 
 export default {
+
+  data(){
+    return{
+      msg:'<h1>这是一个变量</h1>',
+      ok:0,
+      counter:0,
+    }
+  },
+  watch:{
+
+    counter:function(nval,oval){
+
+        console.log('计数器由'+oval+'变成了'+nval);
+
+    }
+
+  },
+  //计算属性
+  computed:{
+      resver_msg(){
+        return this.msg.split('').reverse().join('')
+      }
+  },
   methods: {
     getTarget() {
       return document.querySelector('.right-frame');
